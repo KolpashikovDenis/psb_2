8475b1619de5eb2330bba5ea543870f5f49a7418
KolpaschikovDenis <kolpashikov.denis@gmail.com>
2020-10-24 18:32:21 +0400

    Сделан пункт №3

--- main.py
+++ main.py
@@ -1,18 +1,50 @@
 import sys, re
+import operator
+
 pattern_ip = '[1-9]{1,3}\\.[1-9]{1,3}\\.[1-9]{1,3}\\.([5-9][\\d]|100)'
 pattern_user = '(?<=]\s)(.*?)(?=\s\d)'
 pattern_func = '(?<=\s\/)(.*?)(?=\s)'
 
 def func1(array):
-    user_array = dict()
+    user_dict = dict()
     for line in array:
+        # Получаю из каждой строки массива юзера и вызванную им функцию
+        u = re.search(pattern_user, line)[0]
+        f = re.search(pattern_func, line)[0]
 
+        # тут ищу в словаре вхождение юзера, если есть достаю вложенный словарь с функцией и частотой вызова
+        a = user_dict.get(u)
+        if a:
+            t = dict()
+            b = a.get(f, 0)
+            if b == 0:
+                t[f] = 1
+                a.update(t)
+            else:
+                t[f] = b + 1
+                a.update(t)
+        else:
+            t = dict()
+            t[f] = 1
+            user_dict[u] = t
+    keys = user_dict.keys()
+    for key in keys:
+        a = dict()
+        a = user_dict.get(key)
+        b = max(a, key=a.get)
+        print('Юзер {}, часто используемая функция: {}, кол-во вызовов {}'.format(key, b, a.get(b)))
+
+def func2():
+
+    return
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print('Задайте входной текстовый файл')
         sys.exit(-1)
     array = list()
+
+    # Условие №2
     with open(sys.argv[1]) as file:
         for row in file:
             line = file.readline()
@@ -20,6 +52,9 @@ if __name__ == "__main__":
             if not m:
                 array.append(line)
 
-    # файл прочитан, с учётом условия №2
+    # Условие задания №3
+    func1(array)
 
-    print(str(len(array)))
+    # Условие задания №4
+    func2()
+
